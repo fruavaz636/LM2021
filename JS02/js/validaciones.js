7c70179694a209b9a80f6e9e2d3fb76d51bc75bb
fruavaz636 <fruavaz636@iestriana.com>
2021-03-19 10:08:48 +0100

    JS02

--- JS02/js/validaciones.js
+++ JS02/js/validaciones.js
@@ -19,3 +19,46 @@ function mostrarError(id, mensaje) {
 function borrarError(id) {
     document.getElementById("sp" + id).innerHTML = "";
 }
+
+function mostrarEdad(campo) {
+    document.getElementById("sp" + campo.id).innerHTML = campo.value;
+}
+
+function validarFormulario() {
+    var cadenaErrores = "";
+
+    var cp = document.getElementById("postal");
+    if (cp.value == null || cp.value.length == 0) {
+        cadenaErrores = "<li>Error Cód. Postal: Campo Vacío</li>";
+    } else if (cp.value.length != 5) {
+        cadenaErrores = cadenaErrores 
+                            + "<li>Error Cód. Postal: 5 cifras</li>";
+    } else if (isNaN(cp.value)) {
+        cadenaErrores = cadenaErrores 
+                            + "<li>Error Cód. Postal: No es número</li>";
+    }
+
+    var tel = document.getElementById("telefono");
+    if (tel.value == null || tel.value.length == 0) {
+        cadenaErrores = cadenaErrores 
+                            + "<li>Error Teléfono: Campo Vacío</li>";
+    } else if (tel.value.length != 9) {
+        cadenaErrores = cadenaErrores 
+                            + "<li>Error Teléfono: 9 cifras</li>";
+    } else if (!tel.value.startsWith(9) && !tel.value.startsWith(6)) {
+        cadenaErrores = cadenaErrores 
+                            + "<li>Error Teléfono: 9 o 6 primera cifra</li>";
+    }
+
+    if (cadenaErrores.length == 0) {
+        return true;
+    } else {
+        verErrores(cadenaErrores);
+        return false;
+    }
+}
+
+function verErrores(mensaje) {
+    document.getElementById("errores").innerHTML = mensaje;
+    document.getElementById("capaerrores").style.visibility = "visible";
+}
