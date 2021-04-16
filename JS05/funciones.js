1128490fa25decb0b49e69840b7cbcca4e56c33b
fruavaz636 <fruavaz636@iestriana.com>
2021-04-16 10:07:08 +0200

    Colores

--- JS05/funciones.js
+++ JS05/funciones.js
@@ -16,3 +16,48 @@ function mostrarTablas(numero) {
 
     resultado.innerHTML = texto;
 }
+
+
+function comprobarPrimo() {
+    var numP = document.getElementById("numP").value;
+
+    var resultado = document.getElementById("resultadoPrimo");
+    if (esPrimo(numP)) {
+        resultado.innerHTML = "<p>" + numP + " es Primo</p>";
+    } else {
+        resultado.innerHTML = "<p>" + numP + " no es Primo</p>";
+    }
+}
+
+function esPrimo(numP) {
+    for (let i = 2; i < numP; i++) {
+        if (numP%i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function init() {
+    var nombre = prompt("Usuario:");
+
+    var contenedor = document.getElementById("container");
+
+    if (nombre == "yo") {
+        contenedor.className = "usuario";
+    } else if (nombre == "tu") {
+        contenedor.className = "parner";
+    } else {
+        contenedor.className = "negro";
+    }
+}
+
+//window.addEventListener("load", init, false);
+
+function cambiarFondo() {
+    var rojo = document.getElementById("rojo").value;
+    var verde = document.getElementById("verde").value;
+    var azul = document.getElementById("azul").value;
+
+    document.getElementById("container").style = "background-color:" + "#" + rojo + verde + azul;
+}
